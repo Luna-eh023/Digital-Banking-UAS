b207524b25b00b05f01e74bb64a4eb54151af615
midnght228 <bungaapriyanti336@gmail.com>
2026-06-03 00:42:01 +0700

    //IMPORTANT: GERARD JOBDESK 1st PASS (still stuff to change like the databse)

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PayController;
+use App\Http\Controllers\StatusController;
+
 
 Route::get('/', function () {
     return redirect('/posts');
 });
 
 Route::resource('posts', PostController::class);
+Route::get('/payment', [PayController::class, 'index']);
+Route::post('/payment', [PayController::class, 'store']);
+Route::get('/status', [StatusController::class, 'index']);
