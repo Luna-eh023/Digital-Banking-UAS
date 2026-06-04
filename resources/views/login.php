89a3eeaf208ab061b34cdb15f7016b4023a65325
Charlie <widyahirosaky@widyas-MacBook-Air.local>
2026-06-05 00:26:13 +0700

    charlie lagi

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+//masih update
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,5 +38,4 @@
     </div>
     <script src="login.js"></script>
 </body>
-
 </html>
